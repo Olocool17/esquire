4f99477008c84fb03f9e23aa6c1afad533aa2db6
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2021-04-08 20:01:56 +0200

    Put commands into Cog

--- esquire.py
+++ esquire.py
@@ -31,55 +31,8 @@ class Esquire(commands.Bot):
         self.config = jsonhandler.JsonHandler('config.json')
         self.command_prefix = self.config.get('command_prefixes')
         super(Esquire, self).__init__(self.command_prefix)
-        self.commandsinit()
+        self.add_cog(BasicCommands(self))
         self.initialise()
-
-    def commandsinit(self):
-        @self.event
-        async def on_ready():
-            for channel in self.get_all_channels():
-                if isinstance(channel, discord.TextChannel) and str(
-                        channel) in self.config.get('allowed_text_channels'):
-                    await channel.send("Esquire initialised.")
-
-        @self.command()
-        async def hello(ctx):
-            await ctx.send("ENEMY ARN DETECTED")
-
-        @self.command()
-        async def wobbify(ctx, *args):
-            if len(ctx.message.attachments) != 0:
-                for a in ctx.message.attachments:
-                    if a.filename[-4:] == '.txt':
-                        txtbytes = await a.read()
-                        wobbifiedtxtbytes = wobbifytxt(txtbytes)
-                        wobbifiedfile = discord.File(fp=wobbifiedtxtbytes,
-                                                     filename=a.filename[:-4] +
-                                                     '_wobbified.txt')
-                        await ctx.send(file=wobbifiedfile)
-            else:
-                argstr = " ".join(args)
-                await ctx.send(wobbifystring(argstr))
-
-        @self.command()
-        async def deletelast(ctx,
-                             member: typing.Optional[discord.Member] = None,
-                             covert: Argboolparse('covert') = False):
-
-            messages = ctx.history().filter(lambda x: not x.author.bot)
-            if member != None:
-                async for m in messages:
-                    if (m.author == member and m != ctx.message):
-                        message = m
-                        break
-            else:
-                messages = await messages.flatten()
-                message = messages[1]
-            await message.delete()
-            if covert:
-                await ctx.message.delete()
-            else:
-                await ctx.send("NOTHING TO SEE HERE. MOVE ALONG.")
 
     def initialise(self):
         try:
@@ -122,3 +75,55 @@ class Esquire(commands.Bot):
             log.warn("Encountered an error in cleanup.")
         finally:
             raise exceptions.ExitSignal
+
+
+class BasicCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for channel in self.bot.get_all_channels():
+            if isinstance(channel, discord.TextChannel) and str(
+                    channel) in self.bot.config.get('allowed_text_channels'):
+                await channel.send("Esquire initialised.")
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send("ENEMY ARN DETECTED")
+
+    @commands.command()
+    async def wobbify(self, ctx, *args):
+        if len(ctx.message.attachments) != 0:
+            for a in ctx.message.attachments:
+                if a.filename[-4:] == '.txt':
+                    txtbytes = await a.read()
+                    wobbifiedtxtbytes = wobbifytxt(txtbytes)
+                    wobbifiedfile = discord.File(fp=wobbifiedtxtbytes,
+                                                 filename=a.filename[:-4] +
+                                                 '_wobbified.txt')
+                    await ctx.send(file=wobbifiedfile)
+        else:
+            argstr = " ".join(args)
+            await ctx.send(wobbifystring(argstr))
+
+    @commands.command()
+    async def deletelast(self,
+                         ctx,
+                         member: typing.Optional[discord.Member] = None,
+                         covert: Argboolparse('covert') = False):
+
+        messages = ctx.history().filter(lambda x: not x.author.bot)
+        if member != None:
+            async for m in messages:
+                if (m.author == member and m != ctx.message):
+                    message = m
+                    break
+        else:
+            messages = await messages.flatten()
+            message = messages[1]
+        await message.delete()
+        if covert:
+            await ctx.message.delete()
+        else:
+            await ctx.send("NOTHING TO SEE HERE. MOVE ALONG.")
